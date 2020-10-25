c9ea30db0fa26468f67bcd5a5d649aa5f782c190
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-10-25 15:08:34 +0000

    Automatically backed up by Learn

---
+++ kaelin-cli-project/lib/kaelin/cli/project.rb
@@ -0,0 +1,10 @@
+require "kaelin/cli/project/version"
+
+module Kaelin
+  module Cli
+    module Project
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ kaelin-cli-project/lib/kaelin/cli/project/version.rb
@@ -0,0 +1,7 @@
+module Kaelin
+  module Cli
+    module Project
+      VERSION = "0.1.0"
+    end
+  end
+end
